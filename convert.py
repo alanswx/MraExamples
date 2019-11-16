12dce0d72e238289e9e9c54659fa108648be22f2
Alan Steremberg <alan.steremberg@gmail.com>
2019-11-15 16:19:23 -0800

    added functionality

--- convert.py
+++ convert.py
@@ -1,6 +1,7 @@
 import sys
 import re
 import zipfile
+import os
 
 def lreplace(pattern, sub, string):
     """
@@ -64,15 +65,96 @@ def load_file(filename):
 def process_roms(romlist):
     print(romlist[0]['ofile'].split('.'))
     rbfname=romlist[0]['ofile'].split('.')[1]
+    rbfname=rbfname.capitalize()
+    if (rbfname=="Dkong"):
+        rbfname="DonkeyKong"
+    elif (rbfname=="Alibbt"):
+        rbfname="Alibaba"
+    elif (rbfname=="Asteroid"):
+        rbfname="Asteroids"
+    elif (rbfname=="Azurn"):
+        rbfname="AzurianAttack"
+    elif (rbfname=="Blckhl"):
+        rbfname="BlackHole"
+    elif (rbfname=="Bmbjck"):
+        rbfname="BombJack"
+    elif (rbfname=="Brubbr"):
+        rbfname="BurningRubber"
+    elif (rbfname=="Btime"):
+        rbfname="BurgerTime"
+    elif (rbfname=="Bwidow"):
+        rbfname="BlackWidow"
+    elif (rbfname=="Cclimb"):
+        rbfname="CrazyClimber"
+    elif (rbfname=="Ckong"):
+        rbfname="CrazyKong"
+    elif (rbfname=="Crush"):
+        rbfname="CrushRoller"
+    elif (rbfname=="Csmvng"):
+        rbfname="CosmicAvenger"
+    elif (rbfname=="Ctcomb"):
+        rbfname="Catacomb"
+    elif (rbfname=="Dorodn"):
+        rbfname="Dorodon"
+    elif (rbfname=="Drmshp"):
+        rbfname="DreamShopper"
+    elif (rbfname=="Frggr"):
+        rbfname="Frogger"
+    elif (rbfname=="Galaxn"):
+        rbfname="Galaxian"
+    elif (rbfname=="Gorkns"):
+        rbfname="Gorkans"
+    elif (rbfname=="Ladybg"):
+        rbfname="LadyBug"
+    elif (rbfname=="Lizwiz"):
+        rbfname="LizardWizard"
+    elif (rbfname=="Mspcmn"):
+        rbfname="MsPacman"
+    elif (rbfname=="Orbtrn"):
+        rbfname="Orbitron"
+    elif (rbfname=="Pacclb"):
+        rbfname="PacmanClub"
+    elif (rbfname=="Pacpls"):
+        rbfname="PacmanPlus"
+    elif (rbfname=="Phnx"):
+        rbfname="Phoenix"
+    elif (rbfname=="Ponpok"):
+        rbfname="Ponpoko"
+    elif (rbfname=="Scrmbl"):
+        rbfname="Scramble"
+    elif (rbfname=="Snpjck"):
+        rbfname="SnapJack"
+    elif (rbfname=="Sprglb"):
+        rbfname="SuperGlob"
+    elif (rbfname=="Tmplt"):
+        rbfname="TimePilot"
+    elif (rbfname=="Vvcar"):
+        rbfname="VanVanCar"
+    elif (rbfname=="Warbug"):
+        rbfname="WarOfTheBugs"
+    elif (rbfname=="Wdpckr"):
+        rbfname="Woodpecker"
+    elif (rbfname=="Xevs"):
+        rbfname="Xevious"
+    count = 0
     for rom in romlist:
         ofilename=rom['ofile'].replace('.rom','.mra')
+        if count==0:
+            ofiledir='_newarcade/'
+            os.makedirs(ofiledir,exist_ok=True)
+            ofilename=ofiledir+rbfname+'.mra'
+        else:
+            ofiledir='_newarcade/_hacks/_'+rbfname+'/'
+            os.makedirs(ofiledir,exist_ok=True)
+            ofilename=ofiledir+ofilename
+
         with open(ofilename,"w") as fp:
             zf1= zipfile.ZipFile(rom['zip1'], 'r')
             try:
               zf2= zipfile.ZipFile(rom['zip2'], 'r')
               mrazip2=lreplace('MAME/','',rom['zip2'])
             except KeyError:
-              print 'ERROR: Did not find zip2'
+              print ('ERROR: Did not find zip2')
 
             mrazip1=lreplace('MAME/','',rom['zip1'])
 
@@ -80,25 +162,46 @@ def process_roms(romlist):
             fp.write('  <rbf>{}</rbf>\n'.format(rbfname))
             fp.write('  <rom index="0" zip="{}" md5="{}">\n'.format(mrazip1,rom['md5']))
             for part in rom['ifiles']:
-               try:
+
+               if part[0]=='"':
+                   part=part.strip('"')
+               if part[0]=='.' and part[1]=='.':
+                   rfname=part.replace('../','')
+                   print('GOT REAL FILE {}'.format(rfname))
+                   with open(rfname,'rb') as rfp:
+                       fp.write('      <part dt:dt="bin.hex">\n')
+                       hexcount=1
+                       byte = rfp.read(1)
+                       while byte:
+                        fp.write(byte.hex()+' ')
+                        if (not (hexcount %16)):
+                          fp.write('\n')
+                        byte=rfp.read(1)
+                        hexcount=hexcount+1
+                       fp.write('      </part>\n')
+               else:
+                try:
                  info = zf1.getinfo(part)
-               except KeyError:
-                 print 'ERROR: Did not find %s in zip file' % part 
+                except KeyError:
+                 print ('ERROR: Did not find %s in zip file' % part )
                  try:
                    info = zf2.getinfo(part)
                  except KeyError:
-                   print 'ERROR: Did not find %s in zip file' % part 
+                   print ('ERROR: Did not find %s in zip file' % part )
                  else:
-                   print '%s is %d bytes' % (info.filename, info.file_size)
+                   print ('%s is %d bytes' % (info.filename, info.file_size))
                    fp.write('        <part zip="{}" name="{}"/>\n'.format(mrazip2,part))
-               else:
-                 print '%s is %d bytes' % (info.filename, info.file_size)
+                else:
+                 print ('%s is %d bytes' % (info.filename, info.file_size))
                  fp.write('        <part name="{}"/>\n'.format(part))
             fp.write('  </rom>')
             fp.write('  </misterromdescription>')
         print(rom)
+        count=count+1
 
 
 if __name__ == "__main__":
-    romlist=load_file(sys.argv[1])
-    process_roms(romlist)
+    for arg in sys.argv:
+        if (arg!=sys.argv[0]):
+            romlist=load_file(arg)
+            process_roms(romlist)
